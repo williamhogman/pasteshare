06876c0982f3ea50e6f0ac84e13a45f4dabe6297
William Högman <william@hogman.me>
2012-02-24 23:09:07 +0100

    added settings

--- pasteshare/app.py
+++ pasteshare/app.py
@@ -4,5 +4,12 @@ Main application
 
 import tornado.web
 
+import pasteshare.index as index
+
+_settings = {
+    "template_path": "templates"
+}
+
 application = tornado.web.Application([
-    ])
+    ("/", index.IndexHandler)
+    ],**_settings)
