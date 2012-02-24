301db00e33155254072d9d8e95e9210e2c30ad75
William Högman <william@hogman.me>
2012-02-24 21:24:33 +0100

    Added saving

--- pasteshare/model.py
+++ pasteshare/model.py
@@ -5,19 +5,40 @@ from brukva.adisp import process,async
 
 class Snippet(object):
 
-    def __init__(self,_id,title,content,language,
-                 creation,lastedit=None,author=0):
+    def __init__(self,title,content,language,
+                 creation,views=0,lastedit=None,author=0,_id=-1):
         
-        self._id = _id
+        self.id = _id
         self.title = title
         self.content  = content
         self.language = language
         self.author = author
         self.creation = creation
+
         if lastedit is None:
-            lastedit = creation
-            
+            self.lastedit = creation
+
+    @property
+    def _key(self):
+        return "Snippet:{}".format(self.id)
         
+    @process
+    def save(self):
+        if self.id == -1:
+            self.id = yield self._get_unused_id()
+        key = self._key
+        cli = data.get_client()
+        
+        yield cli.async.hmset(self._key,{
+            "title": self.title,
+            "content": self.content,
+            "language": self.language,
+            "author": self.author,
+            "creation": self.creation
+            })
+                        
+            
+    
     @staticmethod
     @async
     def _get_unused_id(callback):
@@ -30,5 +51,4 @@ class Snippet(object):
     def new(cls,title,content,language,author=0):
         """ Creates a new snippet """
         creation = int(time.time())
-        _id = _get_unused_id()
-        return cls(_id,title,content,language,author,creation)
+        return cls(title,content,language,author,creation)
